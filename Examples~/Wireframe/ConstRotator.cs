02c3ac9826f47e7f3b7c5891db033c452142ec01
Nakata Nobuyuki <nobu.nakata@gmail.com>
2024-02-02 15:16:55 +0900

    add exmples in package

---
+++ Examples~/Wireframe/ConstRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstRotator : MonoBehaviour {
+	public Vector3 speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation *= Quaternion.Euler(Time.deltaTime * speed);
+
+	}
+}
